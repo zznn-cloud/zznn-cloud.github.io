bc65710f62bd464015014203b9bb568bc6551d9f
zznn-cloud <142100400+zznn-cloud@users.noreply.github.com>
2024-01-20 07:10:54 +0000

    Update randombg.js Updated By Github Actions

--- js/randombg.js
+++ js/randombg.js
@@ -1,6 +1,8 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
   "url(/img/bg1.jpg)",
+  "url(/img/橘子.jpg)",
+  "url(/img/海鸥.jpg)",
   "url(/img/bg8.jpg)"
   ];
   //获取背景图片总数，生成随机数
@@ -10,6 +12,8 @@ var backimg =[
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
     "url(/img/bg1.jpg)",
+    "url(/img/橘子.jpg)",
+    "url(/img/海鸥.jpg)",
     "url(/img/bg8.jpg)"
   ];
   //获取banner图片总数,生成随机数
