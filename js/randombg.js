2f070f7a1802728d43846442a3baacd601bb229a
zznn-cloud <142100400+zznn-cloud@users.noreply.github.com>
2024-02-15 04:39:56 +0000

    Update randombg.js Updated By Github Actions

--- js/randombg.js
+++ js/randombg.js
@@ -1,12 +1,13 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(/img/bg1.jpg)",
   "url(/img/橘子.jpg)",
-  "url(/img/狗尾巴草.png)",
+  //"url(/img/狗尾巴草.png)",
   "url(/img/流星.jpg)",
   "url(/img/落日.jpg)",
-  "url(/img/猫咪.jpg)",
-  "url(/img/bg8.jpg)"
+  //"url(/img/猫咪.jpg)",
+  //"url(/img/bg8.jpg)",
+  "url(/img/bg1.jpg)",
+  "url(/img/云彩.jpg)"
   ];
   //获取背景图片总数，生成随机数
   var bgindex =Math.floor(Math.random() * backimg.length);
@@ -14,13 +15,14 @@ var backimg =[
   document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
-    "url(/img/bg1.jpg)",
     "url(/img/橘子.jpg)",
-    "url(/img/狗尾巴草.png)",
+    //"url(/img/狗尾巴草.png)",
     "url(/img/流星.jpg)",
     "url(/img/落日.jpg)",
-    "url(/img/猫咪.jpg)",
-    "url(/img/bg8.jpg)"
+    //"url(/img/猫咪.jpg)",
+    //"url(/img/bg8.jpg)",
+    "url(/img/bg1.jpg)",
+    "url(/img/云彩.jpg)"
   ];
   //获取banner图片总数,生成随机数
   var bannerindex =Math.floor(Math.random() * bannerimg.length);
