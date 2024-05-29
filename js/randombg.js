8837ca21fad83bfd6052be5ac279424a3b3ae6d4
zznn-cloud <142100400+zznn-cloud@users.noreply.github.com>
2024-05-29 07:16:22 +0000

    Update randombg.js Updated By Github Actions

--- js/randombg.js
+++ js/randombg.js
@@ -35,7 +35,8 @@ var backimg =[
   "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-l8oody.png)",
   "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-7pkqxo.jpg)",
   "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E7%A7%91%E6%8A%80%E5%A3%81%E7%BA%B8/wallhaven-yxlvdd.jpg)",
-  "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-o5mpm7.png)"
+  "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-o5mpm7.png)",
+  "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E7%A7%91%E6%8A%80%E5%A3%81%E7%BA%B8/wallhaven-yxl9rk.png)"
   //"url(/img/桃花.jpg)"
   ];
   //获取背景图片总数，生成随机数
@@ -79,7 +80,8 @@ var backimg =[
     "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-l8oody.png)",
     "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-7pkqxo.jpg)",
     "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E7%A7%91%E6%8A%80%E5%A3%81%E7%BA%B8/wallhaven-yxlvdd.jpg)",
-    "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-o5mpm7.png)"
+    "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E5%8A%A8%E6%BC%AB/%E6%B8%A9%E9%A6%A8/wallhaven-o5mpm7.png)",
+    "url(https://drive.wn-apple-teawine.fun/api/raw/?path=/%E7%94%B5%E8%84%91%E5%A3%81%E7%BA%B8%F0%9F%8D%A1/%E7%A7%91%E6%8A%80%E5%A3%81%E7%BA%B8/wallhaven-yxl9rk.png)"
     //"url(/img/桃花.jpg)"
   ];
   //获取banner图片总数,生成随机数
